8fbbe60ac38f87066fe6c7b80d986609fff457d3
byte <118949529+blubware@users.noreply.github.com>
2023-02-10 13:54:27 -0800

    Add files via upload

--- vc_bomber/main.py
+++ vc_bomber/main.py
@@ -3,16 +3,20 @@ from discord.ext import commands
 def vc_bomber():
 
     tokens = open('tokens.txt'.strip()).readlines()
-    channel_id = input(f'VCID > ')
+    channel_id = input('VCID > ')
     amount = 1
 
     with open('start.bat', 'w') as startall:
         startall.write(f'cd zombies\n')
 
+    
     for token in tokens:
+        rbracket = '{'
+        lbracket = '}'
+
         content = f'''
 from discord.ext import commands
-
+import asyncio
 selfbot = commands.Bot(command_prefix='>>', help_command=None, self_bot=True)
 selfbot.remove_command('help')
 
@@ -23,21 +27,23 @@ async def on_ready():
         print(f'Channel not found')
     
     else:
-        await channel.connect()
+        while True:
+            await channel.connect()
+            voice_client = channel.guild.voice_client
+            await asyncio.sleep(0.5)
+            await voice_client.disconnect()
 
 selfbot.run('{token.strip()}')
 '''
-        with open(f'zombies/client{amount}.py', 'w') as client:
-            client.write(content)
+        with open(f'zombies/zombie{amount}.py', 'w') as zombie:
+            zombie.write(content)
 
         with open('start.bat', 'a') as startall:
-            startall.write(f'start python client{amount}.py\n')
+            startall.write(f'start python zombie{amount}.py\n')
 
         with open('kill.bat', 'w') as killall:
             killall.write('taskkill /im python.exe')
 
         amount+=1
 
-
-    
 vc_bomber()
